1dbfa3b1688b0eae429cef94c35c5eeabbf2f890
Ramo74Dixit <ramodixit76@gmail.com>
2024-10-10 15:53:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="maindiv">
+      <h1>Ratan Tata, industry titan and national icon, dies at 86 </h1>
+      <span>Ratan Tata, who ran Tata Sons for more than 20 years as chairman, died at a Mumbai hospital. </span>
+      <p className='para1'>Ratan Tata, the chairman emeritus of Tata Sons, has died, the group announced in a statement on Wednesday. He was 86. Ratan Tata, who ran the conglomerate for more than 20 years as chairman, had been undergoing intensive care in a Mumbai hospital since Monday.</p>
+      <img src="https://www.hindustantimes.com/ht-img/img/2024/10/09/400x225/Ratan_Tata_1728500693199_1728500693516.JPG" alt="Ratan Ji image"/>
+      <p className='para2'>On Monday, the industrialist had in a social media post dismissed speculation surrounding his health and had said he was undergoing routine medical investigations due to his age.
+
+Prime Minister Narendra Modi paid tribute to Ratan Tata, calling him a visionary business leader, compassionate soul, extraordinary human being.
+
+"Ratan Tata endeared himself to people, thanks to his humility, kindness, unwavering commitment to making society better," PM Modi said in a post on X (formerly Twitter).
+</p>
     </div>
   );
 }
